058d24bc038c33ff2d58ec0ceae0384816e25759
Abdussamad <khanabdussamad32@gmail.com>
2023-07-19 18:36:08 +0530

    for_each

--- map.js
+++ map.js
@@ -12,5 +12,19 @@ for ([i,value ]of map){
 
 console.log(map.size);
 
+let languages = ['javascript','python','c++','go']
+
+languages.forEach(function (s){
+    console.log(s);
+})
+
+let values = [1,2,3,4,5,6,7,8,9,10]
+
+let result = values.filter((val)=>(val>5))
+console.log(result);
+
+
+
+
 
 
