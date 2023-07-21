e75c5d821ea0b12837eea37459d73a5f0cb91066
Abdussamad <khanabdussamad32@gmail.com>
2023-07-21 16:32:46 +0530

    map.

--- map.js
+++ map.js
@@ -1,9 +1,23 @@
-let arr = [1,2,3,4,5,6,7,8,9,10]
+// let arr = [1,2,3,4,5,6,7,8,9,10]
 
-let num = arr.map((a)=>(a+10))
-console.log(num);
+// let num = arr.map((a)=>(a+10))
+// console.log(num);
 
-let each = arr.forEach(function(b){
-    console.log(b+10); 
+// let each = arr.forEach(function(b){
+//     console.log(b+10); 
+// })
+// console.log(each);
+
+
+let values = [4,9,16,25,36,49]
+let root = values.map( (a)=>Math.sqrt(a))
+//console.log(root);
+// output [ 2, 3, 4, 5, 6, 7 ]
+
+let sqrt = [64,81,100,121,144]
+let result = sqrt.map(function (val){
+    return Math.sqrt(val)
+ 
 })
-console.log(each);
+
+console.log(result);
