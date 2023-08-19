bac1d09f71ff1d443073ca616bda47cbf0be3737
Abdussamad <khanabdussamad32@gmail.com>
2023-08-19 22:49:36 +0530

    alert

--- new.js
+++ new.js
@@ -13,67 +13,70 @@ const startOver = document.querySelector('.resultParas');
 const p = document.createElement('p');
 let prev_guess = []
 let guessed_num = 1
+const playgame = true
+let hint = false
 
-let playgame = true
+
 if(playgame){
-  submit.addEventListener('click',function(a){
-    a.preventDefault();
-    const guess = parseInt(userInput.value);
-    valid(guess);
-  })
+  submit.addEventListener('click',function(click){
+    click.preventDefault()
+     const guess = parseInt(userInput.value)
+     valid(guess)
+   
+   })
 }
 function valid(Guess){
   if(isNaN(Guess)){
-    alert("enter a valid value")
+    alert("enter valid value")
   }
   else if(Guess < 1){
-    alert("enter number greater than 1")
+    alert("enter value greater than one")
   }
-  else if(prev_guess.includes(Guess)){
-    show_message("you've entered the repeated values")
-  }
-  else{
+  else {
     prev_guess.push(Guess)
-    if(guessed_num===10){
-      show(Guess)
-      show_message('game over ')
-      endgame();
+    
 
+    if(guessed_num===11){
+      display_message('game over ')
+      endgame();
+      show(Guess)
     }
     else{
-      show(Guess);
-      check(Guess);
+      check(Guess)
+      show(Guess)
     }
   }
-
 }
+function check(gUess){
+  if(gUess===randomNumber){
+    display_message("you won ")
+    endgame();
+  
 
-function check(GUESS){
-  if(GUESS===randomNumber){
-    show_message("you won")
-    endgame()
   }
-  else{
-    show_message("keep trying")
+  else if ( guessed_num === 4){
+    alert('hint its 23')
+   
   }
   
 }
-function show(guessed){
-  userInput.value=""
-  guessSlot.innerHTML += `${guessed},`
+function show(g_uess){
+  userInput.value = ""
+  guessSlot.innerHTML += g_uess+","
   guessed_num++
   remaining.innerHTML = `you have ${11-guessed_num} guesses remaining`
 
 }
-function show_message(g_ussed){
-  lowOrHi.innerHTML = `<h2>${g_ussed}</h2>`
 
+function display_message(message){
+lowOrHi.innerHTML = `<h2>${message}</h2>`
 }
+
 function endgame(){
   userInput.value = "";
-  userInput.setAttribute('disabled','');
-  playgame = false;
+userInput.setAttribute('disabled',"")
+playgame = false
 
 
-
+  
 }
